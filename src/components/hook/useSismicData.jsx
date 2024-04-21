f229dee9d6da50a7486a008ed6b834ec65f7a25d
Steven Alvarado Aguilar <alvaradosteven6@gmail.com>
2024-04-21 13:38:16 -0600

    Add new change to watch a problem

--- src/components/hook/useSismicData.jsx
+++ src/components/hook/useSismicData.jsx
@@ -28,7 +28,7 @@ const useSismicData = () => {
 
       let url;
       console.log(process.env.NODE_ENV);
-      if (process.env.NODE_ENV === 'development' && backendPort !== "https://task-manager-visualization-production.up.railway.app/") {
+      if (process.env.NODE_ENV === 'development' && backendPort !== "https://https://task-manager-production-d1ed.up.railway.app/") {
         console.log(backendPort);
         url = `http://localhost:${backendPort}/api/features?${params}`;
       } else {
